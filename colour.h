596c6a52249c3edb105fd8e4f631e2e561542f1a
j3nny-zhang <jennyzhanng@gmail.com>
2022-07-25 08:45:25 -0400

    create background of scene

--- colour.h
+++ colour.h
@@ -6,6 +6,7 @@
 #include <iostream>
 
 void write_colour(std::ostream &out, colour pixel_colour) {
+
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
         << static_cast<int>(255.999 * pixel_colour.y()) << ' '
